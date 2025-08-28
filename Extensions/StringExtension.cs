7928256b2fb2edfea8af2d51cf7ab072f104b452
MahBoiDeveloper <mah.boi.developer@gmail.com>
2025-08-28 18:50:27 +0300

    Add string extension

---
+++ Extensions/StringExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rampastring.Tools.Extensions;
+
+/// <summary>
+/// 
+/// </summary>
+public static class StringExtension
+{
+    public static Stream ToStream(this string text)
+    {
+        // https://stackoverflow.com/questions/1879395/how-do-i-generate-a-stream-from-a-string
+        var stream = new MemoryStream();
+        var writer = new StreamWriter(stream);
+        writer.Write(text);
+        writer.Flush();
+        stream.Position = 0;
+        return stream;
+    }
+}
