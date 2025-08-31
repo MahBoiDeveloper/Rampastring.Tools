b27d1b88493ac37c98457d6179700ace3e687438
MahBoiDeveloper <mah.boi.developer@gmail.com>
2025-08-31 17:13:01 +0300

    Add comments in options

--- IniSerializationOptions.cs
+++ IniSerializationOptions.cs
@@ -4,22 +4,22 @@ using System.Collections.Generic;
 namespace Rampastring.Tools;
 
 /// <summary>
-/// 
+/// Serialization options struct.
 /// </summary>
 public struct IniSerializationOptions
 {
     /// <summary>
-    /// 
+    /// Name of the section that should be used in serialization.
     /// </summary>
     public string SectionName { get; set; }
 
     /// <summary>
-    /// 
+    /// List of properties that should be ignored in serializing process.
     /// </summary>
     public List<string> IgnoreProperties { get; set; }
 
     /// <summary>
-    /// 
+    /// Set true, if uniniatlized properties should be serialized.
     /// </summary>
     public bool WriteEmptyKeys;
 }
